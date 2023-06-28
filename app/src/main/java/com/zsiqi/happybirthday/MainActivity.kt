20969e0a7a7afcdec5f214fd6b31a39805133351
siqiz <siqi.zhao@outlook.sg>
2023-06-28 21:24:35 +0800

    test

---
+++ app/src/main/java/com/zsiqi/happybirthday/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.zsiqi.happybirthday
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.zsiqi.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HappyBirthdayTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingPreview()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+        )
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            fontSize = 30.sp,
+            fontWeight = FontWeight(500),
+            modifier = Modifier.padding(
+                start = 16.dp,
+                top = 30.dp,
+                end = 16.dp,
+                bottom = 16.dp
+            )
+        )
+        Text(
+            text = from,
+            fontSize = 25.sp,
+            fontWeight = FontWeight(400),
+            modifier  = Modifier
+                .align(
+                    alignment = Alignment.End
+                )
+                .padding(
+                    top = 16.dp,
+                    end = 30.dp
+                )
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HappyBirthdayTheme {
+        BirthdayGreetingWithImage(
+            message = stringResource(R.string.happy_birthday_to_you),
+            from = stringResource(R.string.from)
+        )
+    }
+}
